eab249906d3ccaa894ac58b185972f45ae4eba64
Tolga Uğur <29550889+Tolgaugur@users.noreply.github.com>
2023-02-23 19:14:21 +0300

    added logging in via URL option

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import subprocess
 import pyautogui
 import time
+import pyperclip as pyp
 import pandas as pd
 from datetime import datetime
 
 
-def sign_in(meetingid, pswd):
+def sign_in(meetingid, pswd,meetingurl):
     # Opens up the zoom app
     # change the path specific to your computer
 
@@ -17,11 +18,13 @@ def sign_in(meetingid, pswd):
     time.sleep(5)
     pyautogui.getWindowsWithTitle("Zoom")[0].activate()
     print("Zoom activated on the screen")
-    print(meetingid)
-    if pswd == "":
-        print("No password")
-    else:
-        print(pswd)
+    if meetingurl != "nan":
+        print("Meeting URL: "+meetingurl)
+    if meetingid != "nan":
+        print("Meeting ID: "+meetingid)
+    if pswd != "nan":
+        print("Meeting password: "+pswd)
+    
     time.sleep(3)
     
     # clicks the join button
@@ -29,17 +32,24 @@ def sign_in(meetingid, pswd):
     
     
     join_btn = pyautogui.locateCenterOnScreen('join_button.png',confidence=0.9)
-    print(join_btn)
+    # print(join_btn)
     pyautogui.moveTo(join_btn)
     pyautogui.click()
     
     
     # Type the meeting ID
     meeting_id_btn = pyautogui.locateCenterOnScreen("meeting_id_button.png")
-    print(meeting_id_btn)
+    # print(meeting_id_btn)
     pyautogui.moveTo(meeting_id_btn)
     # pyautogui.click()
-    pyautogui.write(meetingid)
+    if meetingid=="nan":
+        # print("No meeting id")
+        pyp.copy(meetingurl)
+        print("Copied link to clipboard : " + meetingurl)
+        pyautogui.hotkey("ctrl","v")
+        # pyautogui.write(meetingurl)
+    else:
+        pyautogui.write(meetingid)
     time.sleep(3)
     pyautogui.press("enter")
     time.sleep(5)
@@ -64,9 +74,8 @@ def sign_in(meetingid, pswd):
     # Types the password and hits enter
 
     meeting_pswd_btn = pyautogui.locateCenterOnScreen("meeting_pswd.png",confidence=0.9)
-    print("found meeting pswd",meeting_pswd_btn)
+    
     if meeting_pswd_btn is None:
-        print("No password")
         return
     pyautogui.moveTo(meeting_pswd_btn)
     pyautogui.click()
@@ -89,11 +98,16 @@ while True:
         # sign in to Zoom meeting(s) for the current time and day
 
         for index, row in matches.iterrows():
-            m_id = str(row["meetingid"])
+            if row["meetingid"] == "None":
+                m_id = ""
+            else:
+                m_id = str(row["meetingid"])
             if row["meetingpswd"] == "None":
                 m_pswd = ""
             else:
                 m_pswd = str(row["meetingpswd"])
-            sign_in(m_id, m_pswd)
-            time.sleep(20)
+            m_url = row["meetingurl"]
+
+            sign_in(m_id, m_pswd,m_url)
+            time.sleep(46)
             print("signed in to meeting id:", m_id)
